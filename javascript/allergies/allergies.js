95b036cf0fc5f7c8d0da72110edd4804a1fe93f0
Quinn Elder <qre.turing@gmail.com>
2020-08-26 10:14:28 -0600

    Add functionality

--- javascript/allergies/allergies.js
+++ javascript/allergies/allergies.js
@@ -3,16 +3,52 @@
 // convenience to get you started writing code faster.
 //
 
+const allAllergies = {
+ cats: 128,
+ pollen: 64,
+ chocolate: 32,
+ tomatoes: 16,
+ strawberries: 8,
+ shellfish: 4,
+ peanuts: 2,
+ eggs: 1,
+}
+
 export class Allergies {
-  constructor() {
-    throw new Error("Remove this statement and implement this function");
+  constructor(number) {
+    this.number = number;
+    this.allergyList = [];
   }
 
   list() {
-    throw new Error("Remove this statement and implement this function");
+    let num = this.number
+    
+    const newList = (num) => {
+    if(num === 0) {
+      return 
+    }
+
+    for (const allergy in allAllergies) {
+      if(num >= allAllergies[allergy]) {
+        if(this.allergyList.includes(allergy)){
+          return newList(num - allAllergies[allergy])
+        }
+        this.allergyList.unshift(allergy)
+        return newList(num - allAllergies[allergy])
+       }
+     }
+   }
+
+   newList(num)
+   return this.allergyList
   }
 
-  allergicTo() {
-    throw new Error("Remove this statement and implement this function");
+  allergicTo(allergy) {
+    this.list()
+    if(this.allergyList.includes(allergy)) {
+      return true
+    } else {
+      return false
+    }
   }
 }
