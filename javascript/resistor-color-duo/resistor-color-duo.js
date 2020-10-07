75591b871428ed5776beaea3fba97b55406c3b2b
Quinn Elder <qre.turing@gmail.com>
2020-10-07 15:41:08 -0600

    Finish resistor-color-duo tests

---
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -0,0 +1,21 @@
+//
+// This is only a SKELETON file for the 'Resistor Color Duo' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const decodedValue = (colorCode) => {
+  let colorValue = {
+    "black": 0,
+    "brown": 1,
+    "red": 2,
+    "orange": 3,
+    "yellow": 4,
+    "green": 5,
+    "blue": 6,
+    "violet": 7,
+    "grey": 8,
+    "white": 9,
+    }
+    let valueResults = `${colorValue[colorCode[0]]}${colorValue[colorCode[1]]}`
+    return Number(valueResults)
+};
